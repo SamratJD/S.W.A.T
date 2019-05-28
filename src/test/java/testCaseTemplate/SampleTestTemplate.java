0196bb7b0ae2fce6d4be3d40a184457e402038f1
Samrat Nag <SamratY92@gmail.com>
2019-05-28 22:09:13 +0530

    Miscellaneous files updated

--- src/test/java/testCaseTemplate/SampleTestTemplate.java
+++ src/test/java/testCaseTemplate/SampleTestTemplate.java
@@ -36,6 +36,7 @@ public class SampleTestTemplate extends TestBase {
 		// Write test steps below:
 		// ===================================================================================================================================================================
 
+		functions.openURL("Enter URL here");
 	}
 
 	@AfterMethod()
